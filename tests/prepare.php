0c7cdac6054ad2386ff88a54ffa3110fc360874e
KUBO Atsuhiro <kubo@iteman.jp>
2007-07-31 12:12:11 +0000

    - Removed an unused @see tag.

--- tests/prepare.php
+++ tests/prepare.php
@@ -32,7 +32,6 @@
  * @copyright  2007 KUBO Atsuhiro <iteman@users.sourceforge.net>
  * @license    http://www.opensource.org/licenses/bsd-license.php  BSD License (revised)
  * @version    SVN: $Id$
- * @see        PEAR_PackageFileManager2
  * @since      File available since Release 0.5.0
  */
 
