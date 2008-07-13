be20871302f5639fbc8b996fcdf71c187812ac4d
KUBO Atsuhiro <kubo@iteman.jp>
2008-07-13 10:20:55 +0000

    - Prepared release 1.1.0.

--- package.php
+++ package.php
@@ -45,7 +45,33 @@ $apiVersion = '0.3.0';
 $apiStability = 'stable';
 $notes = 'A new release of Piece_ORM is now available.
 
-What\'s New in Piece_ORM 1.1.0';
+What\'s New in Piece_ORM 1.1.0
+
+ * Improved error handling: The behavior of internal error handling has been changed so as to handle only own and "exception" level errors.
+ * Improved Automatic Timestamp: The behavior of insert() has been changed so that the current timestamp to be set to the updated_at field.
+ * Many defect fixes: Many defects related to LOB support and the useMapperNameAsTableName option and a few other defects have been fixed.
+
+See the following release notes for details.
+
+Enhancements
+============ 
+
+- Changed the behavior of object loading so that a field value of an object is always set null if the field value of the database is NULL.
+- Renamed from PIECE_ORM_ERROR_INVOCATION_FAILED to PIECE_ORM_ERROR_CANNOT_INVOKE.
+- Changed the behavior of internal error handling so as to handle only own and "exception" level errors.
+- Changed the behavior of insert() so that the current timestamp to be set to the updated_at field. (Ticket #99)
+
+Defect Fixes
+============
+
+- Fixed a defect that caused an fatal error "PHP Fatal error:  Call to a member function getSource() on a non-object" to be raised when insert()/update() call on an object which contains one or more LOB fields setting null. (Ticket #96) (Piece_ORM_Mapper_Common)
+- Fixed a defect that a LOB field value to be null after invoking update() even if the value is not changed. (Ticket #95) (Piece_ORM_Mapper_Common, Piece_ORM_Mapper_LOB)
+- Fixed the definition of "options". (Ticket #88) (data/piece-orm.yaml)
+- Fixed a defect that caused a LOB value to be damaged after invoking update if the value is not changed on PostgreSQL. (Ticket #97) (Piece_ORM_Mapper_Common)
+- Fixed a defect so that all blob values are set to the same value as the value of the last placeholder in a query if multiple blobs are included in the query. (Piece_ORM_Mapper_Common)
+- Fixed a defect so that any queries with relationships do not work if the useMapperNameAsTableName option is enabled.
+- Fixed a defect that caused an exception to be raised when inserting a record to a table including one or more datetime field which is NOT NULL and *not* has default value if Piece_ORM was used with MDB2_Driver_mysql 1.5.0b1. (Ticket #100) (Piece_ORM_Metadata_Factory)
+- Fixed a defect that caused variable names in a default query to be broken if the useMapperNameAsTableName option was enabled. (Ticket #87) (Piece_ORM_Context)';
 
 $package = new PEAR_PackageFileManager2();
 $package->setOptions(array('filelistgenerator' => 'file',
@@ -59,8 +85,8 @@ $package->setOptions(array('filelistgenerator' => 'file',
 
 $package->setPackage('Piece_ORM');
 $package->setPackageType('php');
-$package->setSummary('An object-relational mapping framework for PHP');
-$package->setDescription('Piece_ORM is an object-relational mapping framework for PHP.
+$package->setSummary('An object-relational mapping framework');
+$package->setDescription('Piece_ORM is an object-relational mapping framework.
 
 Piece_ORM is a simple framework based on the DataMapper pattern, and features stdClass centered approach.');
 $package->setChannel('pear.piece-framework.com');
